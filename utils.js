fd7406b4d6bf30a833aea4fd0fd22886a20a142b
bazmatic <barry@earsman.com>
2015-11-06 15:53:32 +1000

    Added auth headers to allowed headers in cross-domain requests

--- utils.js
+++ utils.js
@@ -9,7 +9,7 @@ var Crypto = require("crypto");
 var allowCrossDomain = function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	res.header('Access-Control-Allow-Headers', 'Content-Type, apiKey, appId');
 
 	next();
 };
