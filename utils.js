63cb494c21851546ee5fb2db81f63e1270c35917
bazmatic <barry@earsman.com>
2015-11-10 09:59:22 +1000

    Added adminKey to allowed headers

--- utils.js
+++ utils.js
@@ -9,7 +9,7 @@ var Crypto = require("crypto");
 var allowCrossDomain = function(req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	res.header('Access-Control-Allow-Headers', 'Content-Type,apiKey,appId,userId,token');
+	res.header('Access-Control-Allow-Headers', 'Content-Type,apiKey,appId,adminKey,userId,token');
 
 	next();
 };
